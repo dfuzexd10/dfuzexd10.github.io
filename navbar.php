e4f40140b172a5d0e956ceab9d11b1e858065050
dfuzexd10 <119406767+dfuzexd10@users.noreply.github.com>
2025-12-28 18:27:34 +0545

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,36 @@
+<header class="header">
+    <div class="container header-inner">
+
+        <div class="logo">
+            Jeevan <span>.</span>
+        </div>
+
+        <nav class="nav">
+            <a href="/dfuzexd10.github.io/index.php"
+               class="<?= ($activePage === 'professional') ? 'active' : '' ?>">
+               Professional
+            </a>
+
+            <a href="https://www.youtube.com/watch?v=xvFZjo5PgG0&list=RDxvFZjo5PgG0&start_radio=1"
+               class="<?= ($activePage === 'personal') ? 'active' : '' ?>">
+               Personal
+            </a>
+
+            <a href="/dfuzexd10.github.io/Contact/contact.php"
+               class="<?= ($activePage === 'contact') ? 'active' : '' ?>">
+               Contact
+            </a>
+
+            <div class="lang-dropdown">
+            <button class="lang-btn">
+                🌐 English <i class="fa-solid fa-chevron-down"></i>
+            </button>
+
+            <div class="lang-menu">
+                <a href="/dfuzexd10.github.io/index.php">English</a>
+                <a href="/dfuzexd10.github.io/np/index.php">नेपाली</a>
+                </div>
+</div>
+
+    </div>
+</header>
